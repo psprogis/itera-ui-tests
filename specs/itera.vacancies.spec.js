13f9c1c1b0d84a0c044328d7012117f7337a36a7
pavlo <pavlo.sprogis@harmonicinc.com>
2018-07-01 13:49:13 +0300

    finish initial version of itera.vacancies.spec

--- specs/itera.vacancies.spec.js
+++ specs/itera.vacancies.spec.js
@@ -2,68 +2,49 @@
 const log = require('log4js').getLogger('spec-logger');
 const { MainPage: IteraMainPage, VacanciesPage } = require('../lib/elements/itera');
 
+const mainPage = new IteraMainPage();
+const vacanciesPage = new VacanciesPage();
+
 describe('Vacancies list', () => {
 
     beforeAll(async () => {
         browser.ignoreSynchronization = true;
 
-        // await IteraMainPage.open();
-        this.mainPage = new IteraMainPage();
-        this.vacanciesPage = new VacanciesPage();
+        await mainPage.open();
+        await mainPage.navigateToVacancies();
     });
 
     it('should be accessible from main page', async () => {
-        // await this.mainPage.navigateToVacancies();
-        // await this.mainPage.switchCountry({country: 'Ukraine'});
-        const title = await this.mainPage.getTitle();
-        expect(title).toBe('Vacancies - Itera', 'got wrong title for Vacancies page');
-
-        const vacancies = await this.vacanciesPage.getAllVacancies();
-        console.log(vacancies);
+        const title = await mainPage.getTitle();
+        expect(title).toBe('Work at Itera - Itera', 'got wrong title for Vacancies page');
     });
 
-    // it('should dispay link to seleniumhq at first position in results', async () => {
-    //     const { regularResults: results } = await SearchResultsPage.getAllResults();
-    //     const SELENIUM_URL = 'https://www.seleniumhq.org/';
-    //
-    //     log.debug('search results: ');
-    //     log.debug(results);
-    //
-    //     expect(results.length).toBeGreaterThan(0, 'found 0 results');
-    //
-    //     const firstUrl = results[0].url;
-    //
-    //     expect(firstUrl).toBe(SELENIUM_URL, 'got wrong first result');
-    // });
-    //
-    // it('should contain wikipedia and habr in results', async () => {
-    //     const { regularResults: results } = await SearchResultsPage.getAllResults();
-    //
-    //     const wikipediaExpectedResult = {
-    //         text: 'Selenium — Википедия',
-    //         url: 'https://ru.wikipedia.org/wiki/Selenium',
-    //     };
-    //
-    //     const habrExpectedResult = {
-    //         text: 'Что такое Selenium? / Хабр',
-    //         url: 'https://habr.com/post/152653/',
-    //     };
-    //
-    //     expect(results).toContain(wikipediaExpectedResult, 'no wikipedia in search results');
-    //     expect(results).toContain(habrExpectedResult, 'no habr in search results');
-    // });
-    //
-    // it('should display featured widget', async () => {
-    //     const { text: widgetText } = await SearchResultsPage.getFeaturedWidgetContent();
-    //     const MINIMUM_LENGTH = 10;
-    //
-    //     log.info(`widget text: ${widgetText}`);
-    //
-    //     expect(widgetText.length).toBeGreaterThan(MINIMUM_LENGTH,
-    //         `widget text should have at least ${MINIMUM_LENGTH} characters`);
-    // });
+    describe('for Ukraine', () => {
+        beforeAll(async () => {
+            await vacanciesPage.switchCountry({ country: 'Ukraine' });
+        });
 
-    afterAll(async () => {
-        // add cleanup if needed
+        it('should contain at least one vacancy', async () => {
+            const vacancies = await vacanciesPage.getAllVacancies();
+
+            log.info(`total number of vacancies: ${vacancies.length}`);
+
+            expect(vacancies.length).toBeGreaterThanOrEqual(1, 'found less than 1 vacancy in Ukraine');
+
+            const vacanciesStr = vacancies
+                .map(vacancy => vacancy.title)
+                .join('\n');
+
+            log.info(`vacancies titles:\n${vacanciesStr}`);
+        });
+
+        it('should contain at least one .NET vacancy', async() => {
+            const vacancies = await vacanciesPage.getAllVacancies();
+            const dotNetRegexp = /\.net/i;
+            const dotNetVacancies = vacancies
+                .filter(vacancy => dotNetRegexp.test(vacancy.title));
+
+            expect(dotNetVacancies.length).toBeGreaterThanOrEqual(1, 'found less than 1 .NET vacany');
+        });
     });
 });
